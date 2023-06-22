097e52228d7cb69e24dd4b320e841b4b6b623f0a
mirkasimovashaxin <137146816+mirkasimovashaxin@users.noreply.github.com>
2023-06-22 13:03:50 +0500

    dfssd
    sdfs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,15 +11,19 @@ export default class App extends Component {
   render() {
     return (
       <div>
-         <Navbar  expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home"><div className="lo"></div></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home"><button className='gg'>Barcha kurslar</button></Nav.Link>
-            
-            <NavDropdown className='sd' title="UZBEK" id="basic-nav-dropdown">
+  <Navbar expand="lg" className="bg-body-tertiary">
+      <Container >
+        <Navbar.Brand href="#"><div className="lo"></div></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+          
+       
+           <NavDropdown className='gg' title="BARCHA KURSLAR" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -28,11 +34,40 @@ export default class App extends Component {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#link" ><button className='sdr'>KIRISH</button></Nav.Link>
           </Nav>
+          <Form className="d-flex">
+                 <NavDropdown className='sd' 
+             title="UZBEK" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+     <Nav.Link href="#link" ><button className='sdr'>KIRISH</button></Nav.Link>
+
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
+
+    <div className="tr">
+      <div className="ye"></div>
+      <div className="kitt">
+        <h1 className='hh'>Biz sizga <span>Dasturla</span> <br />
+va boshqa kurslarni taklif <br /> 
+qilamiz.</h1>
+<p className='pp'>Siz istalgan vaqtda har qanday mavzuda kurslarni o’rganishingiz <br /> 
+mumkin. Buning uchun qidiruv tizimidan foydalaning</p>
+<input className='input' type="text" placeholder='Qanday kurs o’rganmoqchisiz ?' />
+      </div>
+    </div>
+ 
       </div>
     )
   }
---
+++ src/Kotta.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Kotta extends Component {
+  render() {
+    return (
+   <div className="kotta">
+
+    
+   </div>
+    )
+  }
+}
